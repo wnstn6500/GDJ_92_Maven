dd66a61245acb30319444b6fa59deab2bbe07d20
daeki <kimdaeki@gmail.com>
2025-07-31 10:33:58 +0900

    fix

--- src/main/java/com/winter/app/board/BoardDAO.java
+++ src/main/java/com/winter/app/board/BoardDAO.java
@@ -2,6 +2,9 @@ package com.winter.app.board;
 
 public interface BoardDAO {
 	
+	//detail
+	public BoardVO detail(BoardVO boardVO)throws Exception;
+	
 	//insert
 	public int insert(BoardVO boardVO)throws Exception;
 
--- src/test/java/com/winter/app/board/notice/NoticeDAOTest.java
+++ src/test/java/com/winter/app/board/notice/NoticeDAOTest.java
@@ -6,11 +6,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.winter.app.board.BoardVO;
+
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
+@Slf4j
 class NoticeDAOTest {
 	
 	@Autowired
 	private NoticeDAO noticeDAO;
+	
+	@Test
+	void detailTest()throws Exception{
+		NoticeVO noticeVO = new NoticeVO();
+		noticeVO.setBoardNum(1L);
+		BoardVO boardVO = noticeDAO.detail(noticeVO);
+		log.info("result : {}", boardVO);
+		assertNotNull(boardVO);
+	}
 
 	//@Test
 	void insertTest()throws Exception {
