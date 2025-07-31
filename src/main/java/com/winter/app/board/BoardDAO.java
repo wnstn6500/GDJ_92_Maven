8caf86217322187ad8f8a386b7677251430b1f06
daeki <kimdaeki@gmail.com>
2025-07-31 12:18:13 +0900

    fix2

--- src/main/java/com/winter/app/board/BoardDAO.java
+++ src/main/java/com/winter/app/board/BoardDAO.java
@@ -7,5 +7,11 @@ public interface BoardDAO {
 	
 	//insert
 	public int insert(BoardVO boardVO)throws Exception;
+	
+	//update
+	public int update(BoardVO boardVO)throws Exception;
+	
+	//delete
+	public int delete(BoardVO boardVO)throws Exception;
 
 }
