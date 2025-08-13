0b0687b25903352934993ce8d9e0e9765d9cf175
daeki <kimdaeki@gmail.com>
2025-08-13 11:10:59 +0900

    filter

--- src/main/java/com/winter/app/Gdj92MavenApplication.java
+++ src/main/java/com/winter/app/Gdj92MavenApplication.java
@@ -2,8 +2,11 @@ package com.winter.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Gdj92MavenApplication {
 
 	public static void main(String[] args) {
